4f89787c3a42220b6d3bc4cf9252725f2bdce9ab
noah <ssh7636@gmail.com>
2021-12-20 21:59:11 +0900

    spring security config setting

--- security/src/main/java/com/practice/security/controller/IndexController.java
+++ security/src/main/java/com/practice/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.practice.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -12,5 +13,35 @@ public class IndexController {
         //mustache 기본폴더 src/main/resource
         //view resolver 설정 : templates (prefix), .mustache(suffix)
         return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨";
     }
 }
